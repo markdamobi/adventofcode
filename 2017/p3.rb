2d3f5a5e633c3ce40f4a0522a20d78d4a7822db5
Mark Donald Amobi <mamobi@selectrehab.com>
2017-12-13 13:32:01 -0600

    add solution to p3 part 1

--- 2017/p3.rb
+++ 2017/p3.rb
@@ -5,7 +5,6 @@ def bottom_corner(n)
   elsif n == 1
     return 7
   else
-
     a_prev = 7
     a_curr = 0
     2.upto(n) do |i|
@@ -16,3 +15,78 @@ def bottom_corner(n)
   end
   a_curr
 end
+
+class Mover
+  attr_accessor :x,:y
+
+  def initialize(xcoord=0,ycoord=0)
+    @x = xcoord
+    @y = ycoord
+  end
+
+  def move(direction)
+    if direction == :right
+      @x += 1
+    elsif direction == :up
+      @y += 1
+    elsif direction == :left
+      @x -= 1
+    elsif direction == :down
+      @y -= 1
+    end
+  end
+
+  def coord
+    [x,y]
+  end
+end
+
+def getcoord(num)
+  origin = get_origin(num)
+  origin_num = bottom_corner(origin)
+  curr_position = origin_num
+  mover = Mover.new(-origin, -origin)
+  return mover.coord if num == origin_num
+  (2*origin + 1).times do
+    mover.move(:right)
+    curr_position +=1
+    return mover.coord if curr_position == num
+  end
+  (2*origin + 1).times do
+    mover.move(:up)
+    curr_position +=1
+    return mover.coord if curr_position == num
+  end
+  (2*origin + 2).times do
+    mover.move(:left)
+    curr_position +=1
+    return mover.coord if curr_position == num
+  end
+  (2*origin + 2).times do
+    mover.move(:down)
+    curr_position +=1
+    return mover.coord if curr_position == num
+  end
+
+end
+
+def get_origin(num)
+  origin = 0
+  i = 0
+  while true
+    if bottom_corner(i+1) > num
+      origin = i
+      break
+    end
+    i += 1
+  end
+  origin
+end
+
+def get_d(num)
+  num_coord = getcoord(num)
+  num_coord.reduce(0){|acc,c| acc + c.abs}
+end
+
+
+##get_d(326)
