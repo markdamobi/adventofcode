316ad79b013e727b681225434d553bd5523097d9
Mark Donald Amobi <mamobi@selectrehab.com>
2017-12-13 10:40:31 -0600

    solve p2 part2

--- 2017/p2.rb
+++ 2017/p2.rb
@@ -2,6 +2,10 @@ def checksum(matrix)
   matrix.reduce(0){|acc,row| acc + (row.max-row.min)}
 end
 
+def evendiv(matrix)
+  matrix.reduce([]){|acc,row| acc << row.permutation(2).to_a.uniq.find{|x,y| (x%y == 0)}}.reduce(0){|acc, pair| acc + pair[0]/pair[1]}
+end
+
 def read_matrix(file)
   File.readlines(file).map{|l| l.strip.split("\t").map(&:to_i)}
 end
@@ -9,4 +13,8 @@ end
 ## assume irb is called from root.
 def run1
   checksum(read_matrix('2017/p2.txt'))
+end
+
+def run2
+  evendiv(read_matrix('2017/p2.txt'))
 end
