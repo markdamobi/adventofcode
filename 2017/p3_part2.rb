1dfc6d3e322fcd4fbfbeaf6c949db9eb86cffcf0
Mark Donald Amobi <mamobi@selectrehab.com>
2017-12-14 09:33:21 -0600

    add code to properly move one step in 3b

---
+++ 2017/p3_part2.rb
@@ -0,0 +1,46 @@
+class Mover
+  attr_accessor :x,:y, :cannot_move_right, :cannot_move_left
+
+  def initialize(xcoord=0,ycoord=0)
+    @x = xcoord
+    @y = ycoord
+  end
+
+  def move(direction)
+    if direction == :right
+      if y <= 0 && x <= y.abs && x >= y
+        @x += 1
+        return true
+      else
+        @cannot_move_right = true
+      end
+    elsif direction == :up
+      if x > 0 && x > y && cannot_move_right
+        @y += 1
+        return true
+      end
+    elsif direction == :left
+      if y > 0 && x > -y && x <= y
+        @x -= 1
+        return true
+      else
+        @cannot_move_left = true
+      end
+    elsif direction == :down
+      if x < 0 && x < y && cannot_move_left
+        @y -= 1
+        return true
+      end
+    end
+    return false
+  end
+
+  def step
+    (move(:right) || move(:up) || move(:left) || move(:down))
+    # coord
+  end
+
+  def coord
+    [x,y]
+  end
+end
