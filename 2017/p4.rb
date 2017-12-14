161a386bc78e0d34b75ce20683944e0be12c37da
Mark Donald Amobi <markdamobi@gmail.com>
2017-12-14 17:41:30 -0600

    solve problem 4 part 2

--- 2017/p4.rb
+++ 2017/p4.rb
@@ -3,7 +3,12 @@ def read_arr(file)
   File.readlines(file).map{|l| l.strip.split(" ")}
 end
 
-def run
+def run1
   input = read_arr('2017/p4_input.txt')
   input.select{|i| i == i.uniq}.count
+end
+
+def run2
+  input = read_arr('2017/p4_input.txt')
+  input.select{|i| i.count == i.map{|w| w.chars.sort.join("")}.uniq.count }.count
 end
