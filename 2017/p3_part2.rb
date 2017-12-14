7e3d7be86305fe1462646de822d438e348cad245
Mark Donald Amobi <mamobi@selectrehab.com>
2017-12-14 11:19:32 -0600

    solve part 2 of problem 3

--- 2017/p3_part2.rb
+++ 2017/p3_part2.rb
@@ -1,9 +1,11 @@
 class Mover
-  attr_accessor :x,:y, :cannot_move_right, :cannot_move_left
+  attr_accessor :x,:y
+  attr_reader :cannot_move_right, :cannot_move_left, :board
 
   def initialize(xcoord=0,ycoord=0)
     @x = xcoord
     @y = ycoord
+    @board = {[0,0] => 1}
   end
 
   def move(direction)
@@ -39,10 +41,38 @@ class Mover
 
   def step
     (move(:right) || move(:up) || move(:left) || move(:down))
-    # coord
   end
 
   def coord
     [x,y]
+  end
+
+  def coord_val
+    board[coord]
+  end
+
+  def neighbors
+    [[x-1,y],[x+1,y],
+     [x,y-1], [x, y+1],
+     [x-1,y-1],[x+1,y+1],
+     [x-1,y+1],[x+1,y-1]]
+  end
+
+  def step_and_update
+    if step
+      @board[coord] = neighbors.reduce(0){|acc, neighbor| acc + board.fetch(neighbor, 0)}
+    end
+  end
+end
+
+
+def run
+  m = Mover.new(0,0)
+  loop do
+    m.step_and_update
+    if m.coord_val > 361527
+      puts m.coord_val
+      break
+    end
   end
 end
