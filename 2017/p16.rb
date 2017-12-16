de1cc5d7f83a00e50d8d87225edccdd044d7c42c
Mark Donald Amobi <markdamobi@gmail.com>
2017-12-15 23:18:51 -0600

    solve p16 part1

--- 2017/p16.rb
+++ 2017/p16.rb
@@ -1,20 +1,46 @@
 require 'active_support/all'
 require 'byebug'
 def read_file(file)
-  File.readlines(file).map{|l| l.strip }
+  File.read(file).split(',')
 end
 
-class SomeClass
-  attr_reader :something
+class Dance
+  attr_reader :moves, :programs
 
-  def initialize(x)
+  def initialize(dance_moves)
+    @moves = dance_moves
+    @programs = ("a".."p").to_a
+    dance
+  end
 
+  def spin(inst)
+    n = inst[1..-1].to_i
+    @programs = programs[-n..-1] + programs[0...-n]
+  end
+
+  def xchange(inst)
+    a,b = inst[1..-1].split("/").map(&:to_i)
+    @programs[a], @programs[b] = programs[b], programs[a]
+  end
+
+  def partner(inst)
+    a,b = inst[1..-1].split("/")
+    a_i, b_i = programs.index(a), programs.index(b)
+    @programs[a_i], @programs[b_i] = b, a
+  end
+
+  def dance
+    moves.each do |m|
+      spin(m) if m.starts_with? "s"
+      xchange(m) if m.starts_with? "x"
+      partner(m) if m.starts_with? "p"
+    end
   end
 end
 
 
 def run1
-  # s = SomeStuff.new(read_file('2017/p16_txt.txt'))
-  s = SomeStuff.new(read_file('2017/p16_input.txt'))
-  s.some_method
+  # s = SomeStuff.new(read_file('2017/p16_test.txt'))
+  d = Dance.new(read_file('2017/p16_input.txt'))
+  d.programs
 end
