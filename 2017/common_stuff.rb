fec91dbed90827453968aa2a1769918d43841f42
Mark Donald Amobi <markdamobi@gmail.com>
2017-12-15 22:41:54 -0600

    add some skeleton

---
+++ 2017/common_stuff.rb
@@ -0,0 +1,20 @@
+require 'active_support/all'
+require 'byebug'
+def read_file(file)
+  File.readlines(file).map{|l| l.strip }
+end
+
+class SomeClass
+  attr_reader :something
+
+  def initialize(x)
+
+  end
+end
+
+
+def run1
+  # s = SomeStuff.new(read_file('2017/p16_txt.txt'))
+  s = SomeStuff.new(read_file('2017/p16_input.txt'))
+  s.some_method
+end
