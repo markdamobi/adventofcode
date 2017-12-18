3a5bdda5f743b988c5dfe762506843c529254b05
Mark Donald Amobi <markdamobi@gmail.com>
2017-12-18 11:07:21 -0600

    add incorrect solution to 11 part 1

--- 2017/p11.rb
+++ 2017/p11.rb
@@ -1,17 +1,18 @@
 require 'active_support/all'
 require 'byebug'
 def read_file(file)
-  File.read.strip.split(",")
+  File.read(file).strip.split(",")
 end
 
 class Hex
-  attr_reader :directions, :origin, :x, :y
+  attr_reader :directions, :origin, :x, :y, :dist
 
   def initialize(directions)
     @directions = directions
     @origin = [0,0]
     ### note: actual x coordinate needs to be multiplied by square root of 3.
     @x, @y = [0,0]
+    @dist = 0
     # @pointer = [0,0]
 
   end
@@ -38,18 +39,57 @@ class Hex
     end
   end
 
-  def distance(a, b)
+  def ne?
+    x > 0 && y > 0
+  end
 
+  def se?
+    x > 0 && y < 0
+  end
+
+  def nw?
+    x < 0 && y > 0
+  end
+
+  def sw?
+    x < 0 && y < 0
+  end
+
+  def n_or_s?
+    x == 0 && y != 0
+  end
+
+  def e_or_w?
+    x != 0 && y == 9
+  end
+
+  def origin?
+    coord == origin
+  end
+
+  def d_from_origin
+    if e_or_w?
+      @dist = x
+    else
+      @dist = (x.abs + y.abs)/2
+    end
   end
 
   def coord
     [x,y]
   end
+
+  def travel
+    directions.each{|d| move(d.to_sym)}
+  end
 end
 
 
+## this solution passess all the test cases.
+## not sure why it doesn't work for the main input. 
 def run1
-  # h = Hex.new(read_file('2017/p11_test.txt'))
-  h = Hex.new(read_file('2017/p11_input.txt'))
-  s.some_method
+  # hex = Hex.new(read_file('2017/p11_test.txt'))
+  hex = Hex.new(read_file('2017/p11_input.txt'))
+  hex.travel
+  hex.d_from_origin
 end
