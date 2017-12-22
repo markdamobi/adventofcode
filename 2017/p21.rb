7c140d5cd5546e981dfd4b62d6e87a264629c9e7
Mark Donald Amobi <markdamobi@gmail.com>
2017-12-22 15:10:10 -0600

    solve problem 21

--- 2017/p21.rb
+++ 2017/p21.rb
@@ -15,34 +15,11 @@ class Art
 
   end
 
-  # def flip(piece)
-  #   piece.map!{|part| part.reverse}
-  #   piece
-  # end
-  #
-  # def stringify(arr)
-  #   arr.map{|ar| ar.join("")}.join("/")
-  # end
-  #
-  # def rotate(piece)
-  #   piece.map!{|part| part.reverse}
-  #   piece
-  # end
-  #
-  # def get_piece(y1,x1,x2)
-  #   piece = []
-  #   depth = x2 - x1
-  #   0.upto(depth).each do |i|
-  #     piece << board[y1+i][x1..x2]
-  #   end
-  # end
-
   def enhance
     block_size = board.size.even? ? 2 : 3
-    num_of_positions = ( board.size / block_size )^2
+    num_of_positions = ( board.size / block_size )**2
     @new_board = Array.new((board.size / block_size) * (block_size+1) ){Array.new}
-
-    num_of_positions.each do |pos|
+    (0...num_of_positions).each do |pos|
       block = get_block(block_size, pos)
       enhanced_block = enhance_block(block)
       add_to_board(enhanced_block, pos)
@@ -52,7 +29,12 @@ class Art
   end
 
   def enhance_block(block)
-
+    block_orientations = orientations(block)
+    correct_pos = block_orientations.find do |position|
+      rules[stringify(position)].present?
+    end
+    # byebug
+    arrayify(rules[stringify(correct_pos)])
   end
 
   def rotate_array(arr)
@@ -81,25 +63,28 @@ class Art
     r2 = rotate_array(r1)
     r3 = rotate_array(r2)
     f = flip(arr)
+    r1f = flip(r1)
     r2f = flip(r2)
-    [r1,r2,r3,f,r2f]
+    r3f = flip(r3)
+    [arr,r1,r2,r3,f,r1f,r2f,r3f]
   end
 
   def get_block(block_size, pos)
     block = Array.new(block_size){Array.new}
     start_y, start_x = get_start_coord(block_size, board.size, pos)
-    num_squares = (block_size)^2
+    num_squares = (block_size) ** 2
     num_squares.times do |i|
       curr_y = (i / block_size) + start_y
       curr_x = (i % block_size) + start_x
       block[i / block_size][i % block_size] = board[curr_y][curr_x]
     end
+    block
   end
 
   ## this assumes that board is prepared and ready to accept aditions.
   def add_to_board(block, pos)
     start_y, start_x = get_start_coord(block.size, new_board.size, pos)
-    num_squares = (block.size)^2
+    num_squares = (block.size) ** 2
     num_squares.times do |i|
       curr_y = (i / block.size) + start_y
       curr_x = (i % block.size) + start_x
@@ -127,10 +112,11 @@ end
 
 
 def run1
-  # s = SomeStuff.new(read_file('2017/p21_test.txt'))
+  # art = Art.new(read_file('2017/p21_test.txt'))
   art = Art.new(read_file('2017/p21_input.txt'))
+  5.times{art.enhance}
+  p art.num_on
 
-  byebug
-  # 5.times{art.enhance}
-  # art.num_on
+  13.times{art.enhance}
+  p art.num_on
 end
