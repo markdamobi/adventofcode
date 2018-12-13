a40b05d0f2e536ca6667ca64088fe8109df2506d
Mark Donald Amobi <markdamobi@gmail.com>
2018-12-12 18:26:36 -0600

    solve problem 11

--- 2018/p11.rb
+++ 2018/p11.rb
@@ -1,6 +1,7 @@
 require './helper'
 
 def cell_power(x, y, s_num)
+	# return 1
 	rack_id = x+10
 	p_level = rack_id * y
 	p_level += s_num 
@@ -36,47 +37,18 @@ def part1(s_num)
 	max
 end
 
-# def part2(s_num, grid_size)
-# 	n = 3
-# 	max = [0,0,0]
-# 	max_v = -999999999
-# 	(1..grid_size).each do |size|
-# 		puts "square size: #{size}"
-# 		(1..grid_size-n+1).each do |x|
-# 			(1..grid_size-n+1).each do |y|
-# 				temp_max = get_n_power(x,y,size,s_num)
-# 				if temp_max > max_v
-# 					max = [x,y,size]
-# 					max_v = temp_max
-# 				end
-# 			end
-# 		end
-# 	end
-# 	p max
-# 	max
-# end
-
 
 def part2(s_num, grid_size)
-	n = 3
 	max = [0,0,0]
 	max_v = -999999999
-	(1..grid_size-n+1).each do |x|
-		(1..grid_size-n+1).each do |y|
-			limit = [x,y].min
-			local_max = cell_power(x,y,s_num)
-			if local_max > max_v
-				max_v = local_max 
-				max = [x,y,1]
-			end
-			(2..limit).each do |sq|
-				(x...x+sq).each{|i| local_max += cell_power(i,sq+1,s_num)}
-				(y...y+sq).each{|j| local_max += cell_power(sq+1,j,s_num)}
-				local_max += cell_power(x+sq, y+sq, s_num)
-				if local_max > max_v
-					max = [x,y,sq]
-					max_v = local_max
-				end
+	(1..grid_size).each do |x|
+		puts "x: #{x}. max: #{max.to_s}. max: #{max_v}"
+		(1..grid_size).each do |y|
+			l_max_v, l_max_sq = get_max(x,y,grid_size,s_num)
+			if l_max_v > max_v 
+				max_v = l_max_v
+				max_sq = l_max_sq
+				max = [x,y,max_sq]
 			end
 		end
 	end
@@ -84,4 +56,22 @@ def part2(s_num, grid_size)
 	max
 end
 
+def get_max(x,y,grid_size,s_num)
+	limit = grid_size - [x,y].max + 1
+	max_v = cell_power(x,y,s_num)
+	max_sq = 1 
+	local_max = max_v 
+
+	(2..limit).each do |sq|
+		(x..x+sq-2).each{|i| local_max += cell_power(i,y+sq-1,s_num)}
+		(y..y+sq-2).each{|j| local_max += cell_power(x+sq-1,j,s_num)}
+		local_max += cell_power(x+sq-1, y+sq-1, s_num)
+		if local_max > max_v
+			max_v, max_sq = local_max, sq
+		end
+	end
+	[max_v, max_sq]
+end
+
+## runs in about 4 minis
 # Benchmark.bm{|bm| bm.report {part1(7347,300)}}
