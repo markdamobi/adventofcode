55464558e3c98a41902e2e56f05428bc8a6f7020
Mark Donald Amobi <markdamobi@gmail.com>
2019-12-01 00:25:38 -0600

    updates

--- 2019/template.rb
+++ 2019/template.rb
@@ -1,7 +1,7 @@
 require './helper'
 
 class SomeClass
-  def initialize(filename="pX_input.txt")
+  def initialize(filename="pX.txt")
     @filename = filename
     read_file
   end
@@ -9,15 +9,13 @@ class SomeClass
   attr_reader :filename
 
   def read_file
-    lines = File.readlines(filename).map{ |i| i.chomp}
-    lines.each do |l|
-      process_line(l)
-    end
+    lines = File.readlines(filename).map{ |i| process_line(i) }
+    ## some additional manupulation of lines here.
   end
 
+  ## code to parse eachline.
   def process_line(l)
 
   end
-
 end
 
