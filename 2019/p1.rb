1a2a0b5a4ae99ac1e1954b7e626b4d1e52a54df7
Mark Donald Amobi <markdamobi@gmail.com>
2019-12-01 00:46:17 -0600

    solve part 2 of p1

--- 2019/p1.rb
+++ 2019/p1.rb
@@ -3,12 +3,11 @@ require './helper'
 class P1
   def initialize(filename="p1.txt")
     @filename = filename
-    read_file
   end
 
   attr_reader :filename
 
-  def read_file
+  def read_file1
     lines = File.readlines(filename).map{ |i| process_line(i) }
     lines.reduce(:+)
   end
@@ -16,4 +15,23 @@ class P1
   def process_line(l)
     (l.chomp.to_i / 3) - 2
   end
+end
+
+
+def process(n)
+  (n / 3) - 2
+end
+
+def process_item(i)
+  rs = 0
+  x = i
+  while((x = process(x)) > 0)
+    rs += x
+  end
+  rs
+end
+
+def run2
+  data = File.readlines('p1.txt').map{ |i| i.chomp.strip.to_i }
+  data.map{|i| process_item(i)}.sum
 end
