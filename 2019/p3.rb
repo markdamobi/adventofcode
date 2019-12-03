f48dc94c44072852fdd172ed449790e53aca2e40
Mark Donald Amobi <markdamobi@gmail.com>
2019-12-02 23:31:39 -0600

    add part 1 solution

---
+++ 2019/p3.rb
@@ -0,0 +1,54 @@
+def run1
+  data = File.readlines('p3.txt')
+
+end
+
+class Wire
+  def initialize(path)
+    @moves = path.split(",")
+    @x = 0
+    @y = 0
+    @points = [[0,0]]
+  end
+
+  attr_reader :x, :y, :points, :moves
+
+  def generate_points
+    moves.each{ |m| move(m) }
+  end
+
+
+  def move(m)
+    n = m[1..-1].to_i
+    if(m.start_with?("R"))
+      n.times do
+        @x += 1
+        @points << [x, y]
+      end
+    elsif(m.start_with?("L"))
+      n.times do
+        @x -= 1
+        @points << [x, y]
+      end
+    elsif(m.start_with?("U"))
+      n.times do
+        @y += 1
+        @points << [x, y]
+      end
+    elsif(m.start_with?("D"))
+      n.times do
+        @y -= 1
+        @points << [x, y]
+      end
+    end
+  end
+
+  def intersections(other_wire)
+    points & other_wire.points
+  end
+
+  def min_intersections(other_wire)
+    intersections(other_wire).map{|x,y| x.abs + y.abs}.sort[1]
+  end
+
+end
