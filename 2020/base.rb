4c0fc5f24f89e7d0c2953972844cf4f080adc674
Mark Donald Amobi <markdamobi@gmail.com>
2020-11-30 23:54:34 -0600

    solve p1

---
+++ 2020/base.rb
@@ -0,0 +1,4 @@
+require 'set'
+require 'benchmark'
+require 'pry-byebug'
+require 'active_support/all'
---
+++ 2020/template.rb
@@ -0,0 +1,11 @@
+require './base'
+
+def run1(file: 'pxx.txt')
+  # lines = File.readlines(file).map { |i| do_something(i) }
+
+
+end
+
+def run2(file: 'pxx.txt')
+
+end
