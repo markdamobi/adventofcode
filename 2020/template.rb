df108e2417fe7f2b290399cb3de7202714e7cca1
Mark Donald Amobi <markdamobi@gmail.com>
2020-12-02 23:46:17 -0600

    update template

--- 2020/template.rb
+++ 2020/template.rb
@@ -1,11 +1,22 @@
 require './base'
 
-def run1(file: 'pxx.txt')
-  # lines = File.readlines(file).map { |i| do_something(i) }
-
-
+def part1(file: 'p3_test.txt')
+  input = read_file(file: file)
+  #process input
 end
 
-def run2(file: 'pxx.txt')
+def part2(file: 'p3_test.txt')
+  input = read_file(file: file)
+end
 
+### Helpers
+def read_file(file:)
+  File.readlines(file).map { |line| parse(line) }
+end
+
+def parse(line)
+  line = line.chomp.strip
+
+  # extra manipulation for each line.
+  # eg. line.split(/\W+/) ## split on Non-word characters(i.e returns "words").
 end
